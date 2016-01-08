e16fb656840e9e692cd1de1fcc7a206ded1ef4b9
Willian Pedroso <willianpedroso@prodam.sp.gov.br>
2016-01-08 15:56:24 -0200

    antes de reverter

---
+++ platforms/android/bin/MainActivity-debug-unaligned.apk.d
@@ -0,0 +1,10 @@
+C:\Users\p051234\Documents\Android\Projetos\planejasampa\platforms\android\bin\MainActivity-debug-unaligned.apk : \
+C:\Users\p051234\Documents\Android\Projetos\planejasampa\platforms\android\bin\MainActivity.ap_ \
+C:\Users\p051234\Documents\Android\Projetos\planejasampa\platforms\android\bin\classes.dex \
+C:\Users\p051234\Documents\Android\Projetos\planejasampa\platforms\android\CordovaLib\bin\classes.jar \
+C:\Users\p051234\Documents\Android\Projetos\planejasampa\platforms\android\libs\sqlite4java.jar \
+C:\Users\p051234\Documents\Android\Projetos\planejasampa\platforms\android\libs\arm64-v8a\libsqlite4java-android-aarch64.so \
+C:\Users\p051234\Documents\Android\Projetos\planejasampa\platforms\android\libs\armeabi\libsqlite4java-android-armv5tejl.so \
+C:\Users\p051234\Documents\Android\Projetos\planejasampa\platforms\android\libs\armeabi-v7a\libsqlite4java-android-armv7l.so \
+C:\Users\p051234\Documents\Android\Projetos\planejasampa\platforms\android\libs\x86\libsqlite4java-android-i686.so \
+C:\Users\p051234\Documents\Android\Projetos\planejasampa\platforms\android\libs\x86_64\libsqlite4java-android-x86_64.so \
